a9f517734bc09337ad9cb895f7f7a523e260ac47
danielcalebe <danielcalebe719@gmail.com>
2026-05-28 08:41:36 -0300

    feat: Adicionando dependencias, estilos e assets

--- app/src/main/java/com/medicao0102/braintetris/ui/theme/Type.kt
+++ app/src/main/java/com/medicao0102/braintetris/ui/theme/Type.kt
@@ -2,33 +2,40 @@ package com.medicao0102.braintetris.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.medicao0102.braintetris.R
 
 // Set of Material typography styles to start with
+
+
+val Inter = FontFamily(
+  Font(R.font.b, FontWeight.Bold),
+  Font(R.font.r, FontWeight.Normal),
+  Font(R.font.sm  , FontWeight.SemiBold),
+)
 val Typography = Typography(
   bodyLarge = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = Inter,
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp,
     lineHeight = 24.sp,
     letterSpacing = 0.5.sp
-  )
-  /* Other default text styles to override
+  ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
